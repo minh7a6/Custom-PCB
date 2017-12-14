3f21b2a3c317b63d454524514075033908cc779a
minh7a6 <minhdangle7a6@gmail.com>
2017-12-13 17:48:12 -0700

    Add files via upload

---
+++ LED STRIP ESP8266/example/ledstrip_adafruit.ino
@@ -0,0 +1,50 @@
+// color swirl! connect an RGB LED to the PWM pins as indicated
+// in the #defines
+//Notice D4 because it is pullup pin, so the value may change inversely.
+// public domain, enjoy!
+ 
+#define REDPIN D6
+#define GREENPIN D5
+#define BLUEPIN D4
+ 
+#define FADESPEED 5     // make this higher to slow down
+ 
+void setup() {
+  pinMode(REDPIN, OUTPUT);
+  pinMode(GREENPIN, OUTPUT);
+  pinMode(BLUEPIN, OUTPUT);
+}
+ 
+ 
+void loop() {
+  int r, g, b;
+ 
+  for (r = 0; r < 256; r++) { 
+    analogWrite(REDPIN, r);
+    delay(FADESPEED);
+  } 
+ 
+  for (b = 255; b > 0; b--) { 
+    analogWrite(BLUEPIN, b);
+    delay(FADESPEED);
+  } 
+   for (g = 0; g < 256; g++) { 
+    analogWrite(GREENPIN, g);
+    delay(FADESPEED);
+  } 
+ 
+  for (r = 255; r > 0; r--) { 
+    analogWrite(REDPIN, r);
+    delay(FADESPEED);
+  } 
+ 
+  for (b = 0; b < 256; b++) { 
+    analogWrite(BLUEPIN, b);
+    delay(FADESPEED);
+  } 
+ 
+  for (g = 255; g > 0; g--) { 
+    analogWrite(GREENPIN, g);
+    delay(FADESPEED);
+  } 
+}
